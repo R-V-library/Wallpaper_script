25f32529b32e866b8c03288c5698d0c04ae41ad4
ReinerVerret <rverret99@gmail.com>
2021-11-10 22:51:35 +0100

    File reworked, cleaned comments & removed excess

--- Wallpaper.sh
+++ Wallpaper.sh
@@ -4,7 +4,7 @@
 
 # change directory to directory containing wallpapers
 cd /usr/share/rpd-wallpaper
-echo $PWD # show working directory
+# echo $PWD # show working directory
 
 File=$PWD/Wallpaper.jpg
 FileDate=$(stat -c '%y' $File | cut -d ' ' -f1)
@@ -13,40 +13,41 @@ FileDate=$(stat -c '%y' $File | cut -d ' ' -f1)
 # if file Wallpaper.jpg does not exist or date different from current date => get new wallpaper
 if [[ !(-f $File) || $FileDate != $(date +%F) ]] 
 then 
-	#if [[ '(date +%F)' == '(stat -c '%y' $File | cut -d ' ' -f1)' ]]
-	#if [[ $FileDate == $(date +%F) ]]
-	#then 
-		echo "Wallpaper out of date"
-		echo "Fetching new wallpaper ..."
+	echo "Wallpaper out of date"
+	echo "Fetching new wallpaper ..."
 		
-		# use wget to download Bing.com webpage
-		# save to file wallpaper.html
-		sudo wget bing.com -q -O wallpaper.html
+	# use wget to download Bing.com webpage
+	# save to file wallpaper.html
+	sudo wget bing.com -q -O wallpaper.html
 		
-		# filename contained in html header (<meta property="og:image" .... >)
-		# use regex to extract file url from header
-		filename='wallpaper.html'
-		n=1
-		regex='<meta property="og:image" content="(.*)_tmb.jpg'
-		echo "Regex: $regex"
+	# filename contained in html header (<meta property="og:image" .... >)
+	# use regex to extract file url from header
+	filename='wallpaper.html'
+	regex='<meta property="og:image" content="(.*)_tmb.jpg'
+	# echo "Regex: $regex" # display Regex used 
 
-		while read line; # loop through file
-		do 
-		#echo "Line No. $n : $line"
-		#n=$((n+1))
-	
+	while read line; # loop through file
+	do 
 		if [[ $line =~ $regex ]]
 		then 
-			echo "Found"
+			
+			# signal regex match
+			# echo "Found" 
+			
+			# change url to download full hd image 
 			image="${BASH_REMATCH[1]}"
-			image="${image}_1920x1080.jpg" # change url to download full hd image 
-			echo "$image"
-		break
+			image="${image}_1920x1080.jpg" 
+			
+			# display image URL
+			# echo "$image" 
+			
+			# break after regex match
+			break 
 		fi
 	
 		done < $filename # feed wallpaper.html file into loop 
 		
-		# wallpaper directory : 
+		# raspbian wallpaper directory : /usr/share/rpd-wallpaper
 		# use wget to download wallpaper and save in wallpaper directory
 		sudo wget $image -q -O Wallpaper.jpg
 
@@ -59,70 +60,9 @@ then
 		sudo rm wallpaper.html
 
 else 
+	
+	# Message if wallpaper up to date
 	echo "Wallpaper up to date"
 	echo "Last changed on: $FileDate"
 		
-	#fi
 fi
-
-# use wget to download Bing.com webpage
-# save to file wallpaper.html
-sudo wget bing.com -q -O wallpaper.html
-
-# filename contained in html header (<meta property="og:image" .... >)
-# use regex to extract file url from header
-filename='wallpaper.html'
-n=1
-regex='<meta property="og:image" content="(.*)_tmb.jpg'
-echo "Regex: $regex"
-
-while read line; # loop through file
-do 
-	#echo "Line No. $n : $line"
-	#n=$((n+1))
-	
-	if [[ $line =~ $regex ]]
-	then 
-		echo "Found"
-		image="${BASH_REMATCH[1]}"
-		image="${image}_1920x1080.jpg" # change url to download full hd image 
-		echo "$image"
-		break
-	fi
-	
-done < $filename # feed wallpaper.html file into loop 
-
-
-#while read line; do
-#echo "Line No. $n : $line"
-#n=$((n+1))
-	#if [[$line =~ $regex]]
-	#then 
-		#echo "OK"
-	#fi
-	##for w in $line;
-	##do 
-		##if [[$line =~ $regex ]]
-		##then 
-			##foto="${BASH_REMATCH[1]}"
-			##echo "${foto}_1920x1080.jpg" # concatenate string to form URL
-			##foto="${foto}_1920x1080.jpg" # store URL to variable
-		##else
-			##echo "$w doesn't match" >&2 # error output in shell
-	##done 
-#done < $filename
-
- 
-
-# wallpaper directory : 
-# use wget to download wallpaper and save in wallpaper directory
-sudo wget $image -q -O Wallpaper.jpg
-
-# change wallpaper via pcmanfm
-path="${PWD}/Wallpaper.jpg"
-echo "$path"
-pcmanfm --set-wallpaper /usr/share/rpd-wallpaper/Wallpaper.jpg #pcmanfm only works !without! sudo 
-
-
-# remove wallpaper.htlm file
-sudo rm wallpaper.html
