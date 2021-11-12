91a7ce3fc1bc6c93cee0d798adc75387f45fc2d8
ReinerVerret <rverret99@gmail.com>
2021-11-12 21:58:00 +0100

    Fix q,v,h options

--- Wallpaper.sh
+++ Wallpaper.sh
@@ -15,6 +15,7 @@ Help()
    echo "-h | -H   	Print this Help."
    echo "-q | -Q	Quiet mode, no output shown."
    echo "-v | -V	Verbose mode."
+   exit 0
 }
 
 # init argument flags 
@@ -31,7 +32,7 @@ while getopts "dDhHqQvV" option; do
 		h|H) # display help
 			#echo "h set"
 			Help;;
-		q|Q) # set description flag
+		q|Q) # set quiet flag, higher priority then verbose flag
 			#echo "q set"
 			q=1;;
 		v|V) # set verbose flag
@@ -44,42 +45,62 @@ done
 
 # change directory to directory containing wallpapers
 cd /usr/share/rpd-wallpaper
-# echo $PWD # show working directory
+
+if [[ $v == 1 && $q == 0 ]]
+then
+	echo "Working directory: $PWD" # show working directory
+fi
 
 File=$PWD/Wallpaper.jpg
-FileDate=$(stat -c '%y' $File | cut -d ' ' -f1)
 
 # check if Wallpaper.jpg exists
 # if file Wallpaper.jpg does not exist or date different from current date => get new wallpaper
-if [[ !(-f $File) || $FileDate != $(date +%F) ]] 
+if [[ !(-e $File) || $(stat -c '%y' $File | cut -d ' ' -f1) != $(date +%F) ]]; 
 then 
-	echo "Wallpaper out of date"
-	echo "Fetching new wallpaper ..."
-		
+	if [[ $q == 0 ]]
+	then
+		echo "Wallpaper out of date"
+		echo "Fetching new wallpaper"
+	fi 		
+	
 	# use wget to download Bing.com webpage
 	# save to file wallpaper.html
-	sudo wget bing.com -q -O wallpaper.html
+	if [[ $v == 1 && $q == 0 ]]
+	then 
+		echo "Downloading html file"
+		sudo wget bing.com -O wallpaper.html
+	else 
+		sudo wget bing.com -q -O wallpaper.html
+	fi
+	
 		
 	# filename contained in html header (<meta property="og:image" .... >)
 	# use regex to extract file url from header
 	filename='wallpaper.html'
 	regex='<meta property="og:image" content="(.*)_tmb.jpg'
-	# echo "Regex: $regex" # display Regex used 
-
+	
+	if [[ $v == 1 && $q == 0 ]]
+	then
+		echo "URL Regex: $regex" # display Regex used 
+	fi 
+	
 	while read line; # loop through file
 	do 
 		if [[ $line =~ $regex ]]
 		then 
-			
-			# signal regex match
-			# echo "Found" 
-			
+						
 			# change url to download full hd image 
 			image="${BASH_REMATCH[1]}"
 			image="${image}_1920x1080.jpg" 
 			
-			# display image URL
-			# echo "$image" 
+			if [[ $v == 1 && $q == 0 ]]
+			then
+				# signal regex match
+				echo "Image URL Found" 
+			
+				# display image URL
+				echo "image URL: $image" 
+			fi
 			
 			# break after regex match
 			break 
@@ -89,20 +110,44 @@ then
 		
 	# raspbian wallpaper directory : /usr/share/rpd-wallpaper
 	# use wget to download wallpaper and save in wallpaper directory
-	sudo wget $image -q -O Wallpaper.jpg
-
+	if [[ $v == 1 && $q == 0 ]]
+	then 
+		echo "Downloading image"
+		sudo wget $image -O Wallpaper.jpg
+	else
+		sudo wget $image -q -O Wallpaper.jpg
+	fi
+	
 	# change wallpaper via pcmanfm
 	path="${PWD}/Wallpaper.jpg"
-	echo "$path"
+	
+	if [[ $v == 1 && $q == 0 ]]
+	then
+		echo "Saving image to: $path"
+	fi
+	
+	if [[ $q == 0 ]]
+	then
+		echo "Changing wallpaper"
+	fi 
+	
 	pcmanfm --set-wallpaper /usr/share/rpd-wallpaper/Wallpaper.jpg #pcmanfm only works !without! sudo 
-
+	
 	# remove wallpaper.htlm file
+	if [[ $v == 1 && $q == 0 ]]
+	then 
+		echo "Cleaning html file"
+	fi
 	sudo rm wallpaper.html
+	# sudo rm Wallpaper.jpg # for debugging purposes only
 
 else 
 	
 	# Message if wallpaper up to date
-	echo "Wallpaper up to date"
-	echo "Last changed on: $FileDate"
+	if [[ $q == 0 ]]
+	then
+		echo "Wallpaper up to date"
+		echo "Last changed on: $(date +%F)"
+	fi
 		
 fi
