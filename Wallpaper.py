213db70d7f1f64705c7702fb2be9392042d1e928
ReinerVerret <rverret99@gmail.com>
2022-01-06 19:57:29 +0100

    Add comments to code

--- Wallpaper.py
+++ Wallpaper.py
@@ -17,11 +17,12 @@ parser.add_argument("-p","-P",action="store_true",default=False,help= "Get wallp
 args = parser.parse_args()
 
 
-# Actual wallpaper fetching
+# Actual wallpaper fetching method
 def update_wallpaper(logger):
 	
 	logger.debug("Downloading html file")
-				
+	
+	# Download bing.com html page to extract image url			
 	if args.v:
 		command = "sudo wget bing.com -O Wallpaper.html"
 		logger.debug(" Command used: %s"% (command)) 
@@ -31,11 +32,13 @@ def update_wallpaper(logger):
 		command = "sudo wget bing.com -q -O Wallpaper.html"
 		os.system(command)
 	
-	
+	# Regex to extract url
 	regex = r"<meta property=\"og:image\" content=\"(.*)_tmb.jpg"	
 	logger.debug("Regex used: "+regex)
 	
 	logger.debug(" Trying to open file: %s"% (os.getcwd() + "/Wallpaper.html"))
+	
+	# Search image URL in html file
 	try:
 		with open("Wallpaper.html",'r') as f:
 			logger.debug("Succesfully opened file: %s"% (os.getcwd() + "/Wallpaper.html"))
@@ -43,32 +46,38 @@ def update_wallpaper(logger):
 				m = re.search(regex,line.strip())
 				if m:
 					logger.debug("Regex match found")
+					
+					# Make image URL from found URL 
 					url = m.group(1)+ "_1920x1080.jpg"
 					logger.info("Found image URL: "+ url)
-					break
+					break	
 			logger.debug("Closing and removing file: %s "% (os.getcwd() + "/Wallpaper.html"))			
 			f.close()
+			
+			# Remove downloaded html page when finished
 			command = "sudo rm " + os.getcwd() + "/Wallpaper.html"
 			logger.debug(" Command used: %s"% (command))
 			os.system(command)
-				
+	
+	# Output error if an error occures
 	except Exception as e:
 		logger.error("An error occured during execution")
 		logger.error(e)
-		
+	
+	# Download image without watermark
 	logger.debug("Downloading image")
 	if args.v:
 		command = "sudo wget " + url + " -O Wallpaper.jpg"
 	else:
 		command = "sudo wget " + url + " -q -O Wallpaper.jpg"
-		
 	os.system(command)
 	logger.debug("File saved in: " + os.getcwd())
-		
+	
+	# Update wallpaper	
 	logger.info("Updating wallpaper")
 	command = "pcmanfm --set-wallpaper " + os.getcwd() + "/Wallpaper.jpg"
 	os.system(command)
-	
+
 def main():
 	
 	# Set default logging level
@@ -87,16 +96,19 @@ def main():
 	if args.p or args.d:
 		logger.info("Wallpaper directory: "+ args.d)
 		os.chdir(args.d)
-
+	
+	# Check if file exists 
+	# if no wallpaper file, fetch new one
 	if not os.path.exists(os.getcwd()+"/Wallpaper.jpg"):
 		logger.debug("File does not exist")
 		update_wallpaper(logger)
 	
+	# File exists
 	else:
+		
+		# Check if file up to date, otherwise fetch new wallpaper
 		logger.debug("File exists")
 		t = os.path.getmtime(os.getcwd() + "/Wallpaper.jpg")
-		# ~ t_mod = datetime.date.fromtimestamp(t).isoformat()
-		# ~ print(t_mod,datetime.date.today().isoformat(),t_mod == datetime.date.today().isoformat())
 		if datetime.date.fromtimestamp(t).isoformat() != datetime.date.today().isoformat(): 
 			logger.info("File out of date")
 			update_wallpaper(logger)
@@ -104,7 +116,6 @@ def main():
 		else:
 			logger.info("File up to date")
 	
-	
 	logger.info("Script finished without errors")
-	
+
 main()
