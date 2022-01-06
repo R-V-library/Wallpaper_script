266b0d052ec5a44089106ce976b97cdec55527dc
ReinerVerret <rverret99@gmail.com>
2022-01-06 20:32:24 +0100

    remove broken -d option + clean

--- Wallpaper.sh
+++ Wallpaper.sh
@@ -1,9 +1,7 @@
-
+#!/bin/bash
 # use bash
-#!/usr/bin/env bash
 
 # init argument flags 
-d=0
 q=0
 v=0
 
@@ -14,70 +12,17 @@ Help()
    echo ""
    echo "Example usage $(basename $0)" 2>&1
    echo ""
-   echo "Syntax: $(basename $0) [-dDhHqQvV]"
+   echo "Syntax: $(basename $0) [-hHqQvV]"
    echo "options:"
-   echo "-d | -D	display description of downloaded wallpaper."
    echo "-h | -H   	Print this Help."
    echo "-q | -Q	Quiet mode, no output shown."
    echo "-v | -V	Verbose mode."
    exit 0
 }
 
-# Download description if non existent 
-getDescription(){
-	
-	if [[ $v == 1 && $q == 0 ]]
-	then 
-		echo "Downloading html file"
-		sudo wget bing.com -O Wallpaper.html
-	else 
-		sudo wget bing.com -q -O Wallpaper.html
-	fi
-	
-	regex='<meta property="og:description" content="(.*)" /><title>'
-	#echo "$regex"
-		
-	while read line; # loop through file
-	do 
-		if [[ $line =~ $regex ]]
-		then 
-			#echo "$line"
-						
-			# change url to download full hd image 
-			description="${BASH_REMATCH[1]}"
-									
-			if [[ ($v == 1 || $d == 1) && $q == 0 ]]
-			then
-			
-				# signal regex match
-				echo "Image description found" 
-				
-				# show image description
-				echo "Image description: $description"		
-			fi
-			
-			# save description to a file
-			# description can be called if needed afterwards
-			# only output when -q && description.txt not found
-			printf "Image description: $description \n" | sudo tee $PWD/description.txt 
-						
-			# break after regex match
-			break 
-		fi
-	
-	done < $PWD/Wallpaper.html # feed wallpaper.html file into loop 
-	
-	# remove html file
-	sudo rm $PWD/Wallpaper.html
-	
-}
-
 # Handle arguments via getopts
-while getopts "dDhHqQvV" option; do
+while getopts "hHqQvV" option; do
 	case $option in 
-		d|D) # set description flag
-			#echo "d set"
-			d=1;;
 		h|H) # display help
 			#echo "h set"
 			Help;;
@@ -121,8 +66,7 @@ then
 	else 
 		sudo wget bing.com -q -O Wallpaper.html
 	fi
-	
-		
+			
 	# filename contained in html header (<meta property="og:image" .... >)
 	# use regex to extract file url from header
 	filename='Wallpaper.html'
@@ -148,7 +92,7 @@ then
 				echo "Image URL Found" 
 			
 				# display image URL
-				echo "image URL: $image" 
+				echo "Image URL: $image" 
 			fi
 			
 			# break after regex match
@@ -156,10 +100,7 @@ then
 		fi
 	
 	done < $filename # feed wallpaper.html file into loop 
-	
-	# fetch description of image
-	getDescription
-		
+				
 	# raspbian wallpaper directory : /usr/share/rpd-wallpaper
 	# use wget to download wallpaper and save in wallpaper directory
 	if [[ $v == 1 && $q == 0 ]]
@@ -186,39 +127,22 @@ then
 	pcmanfm --set-wallpaper /usr/share/rpd-wallpaper/Wallpaper.jpg #pcmanfm only works !without! sudo 
 	
 	# remove wallpaper.htlm file
+	sudo rm Wallpaper.html
 	if [[ $v == 1 && $q == 0 ]]
 	then 
 		echo "Removing html file"
-	fi
+	fi	
 	
-	#sudo rm Wallpaper.html
-	#sudo rm Wallpaper.jpg # for debugging purposes only
-
+	
 else 
 		
 	# Message if wallpaper up to date
 	if [[ $q == 0 ]]
 	then
-	
-		fi 
-		
+			
 		echo "Wallpaper up to date"
 		echo "Last changed on: $(date +%F)"
-				
-		# Show description if file found
-		if [[ $d == 1 || $v == 1 ]]
-		then
-		
-			if [[ -e $PWD/description.txt ]]
-			then
-				cat $PWD/description.txt
-						
-			else
-				echo "No description found, fetching description now"
-				getDescription
-			fi
-		fi
-	fi
-		
+									
+	fi	
 fi
 
